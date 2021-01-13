b5c23a96c972663cfcca4c6d8e0126ed2ee4fdf5
Karl Eyre <karl_eyre@hotmail.com>
2021-01-13 18:34:27 +1100

    Trying to get Raycast working

---
+++ Calming Colour/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject box;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var primaryInput = VRDevice.Device.PrimaryInputDevice;
+
+        if (primaryInput.GetButtonUp(VRButton.One))
+        {
+            box.SetActive(false);
+            //Trigger has been pressed and released
+        }
+
+    }
+}
