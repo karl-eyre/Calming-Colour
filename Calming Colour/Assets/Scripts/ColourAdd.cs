c1df326e026a531415a47d4c7fc4664fd545d723
Karl Eyre <karl_eyre@hotmail.com>
2021-01-17 01:08:00 +1100

    Scene models Added

--- Calming Colour/Assets/Scripts/ColourAdd.cs
+++ Calming Colour/Assets/Scripts/ColourAdd.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class ColourAdd : MonoBehaviour, IPointerClickHandler
+public class ColourAdd : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
 
+    Material curentColour;
+
+    //add new colour
     public void OnPointerClick(PointerEventData eventData)
     {
         var raycastResult = eventData.pointerCurrentRaycast;
 
         var controller = GameObject.Find("VRAvatar").GetComponent<PlayerController>();
-        raycastResult.gameObject.GetComponent<Renderer>().material = controller.colourSelection;
-
-        //raycastResult.gameObject.SetActive(false);
+        if (controller.colourSelection != null)
+        {
+            curentColour = controller.colourSelection;
+            raycastResult.gameObject.GetComponent<Renderer>().material = controller.colourSelection;
+        }
     }
 
+    //Temporary Colour
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        var raycastResult = eventData.pointerCurrentRaycast;
+
+        curentColour = raycastResult.gameObject.GetComponent<Renderer>().material;
+
+        var controller = GameObject.Find("VRAvatar").GetComponent<PlayerController>();
+        raycastResult.gameObject.GetComponent<Renderer>().material = controller.colourSelection;
+    }
+
+    //Reset off of temp colour
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<Renderer>().material = curentColour;
+    }
 }
