ceb7d9f1d26a8b9a6fffad3b8410994f3b176edb
Karl Eyre <karl_eyre@hotmail.com>
2021-01-17 02:34:48 +1100

    2

---
+++ Calming Colour/Assets/Scripts/ColourAdd.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColourAdd : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    Material curentColour;
+
+    //add new colour
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var raycastResult = eventData.pointerCurrentRaycast;
+
+        var controller = GameObject.Find("VRAvatar").GetComponent<PlayerController>();
+        if (controller.colourSelection != null)
+        {
+            curentColour = controller.colourSelection;
+            raycastResult.gameObject.GetComponent<Renderer>().material = controller.colourSelection;
+        }
+    }
+
+    //Temporary Colour
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        var raycastResult = eventData.pointerCurrentRaycast;
+
+        curentColour = raycastResult.gameObject.GetComponent<Renderer>().material;
+
+        var controller = GameObject.Find("VRAvatar").GetComponent<PlayerController>();
+        raycastResult.gameObject.GetComponent<Renderer>().material = controller.colourSelection;
+    }
+
+    //Reset off of temp colour
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<Renderer>().material = curentColour;
+    }
+}
---
+++ Calming Colour/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Material curentColour;
+
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        var raycastResult = eventData.pointerCurrentRaycast;
+
+        curentColour = raycastResult.gameObject.GetComponent<Renderer>().material;
+
+        var controller = GameObject.Find("VRAvatar").GetComponent<PlayerController>();
+        raycastResult.gameObject.GetComponent<Renderer>().material = controller.colourSelection;
+    }
+
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.gameObject.GetComponent<Renderer>().material = curentColour;
+    }
+}
