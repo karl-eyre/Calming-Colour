adbeb205a073f45435bc3d18ca83a66f0c2914bd
Karl Eyre <karl_eyre@hotmail.com>
2021-01-29 16:05:26 +1100

    Colour Placement

--- Calming Colour/Assets/Scripts/ColourSelect.cs
+++ Calming Colour/Assets/Scripts/ColourSelect.cs
@@ -8,27 +8,37 @@ public class ColourSelect : MonoBehaviour, IPointerClickHandler, IPointerDownHan
     public GameObject curentHeldColour;
     public GameObject tileHomer;
 
+    public Material currentMaterial;
+
+    private void Start()
+    {
+        if (currentMaterial == false)
+        {
+            currentMaterial = this.gameObject.GetComponent<Renderer>().material;
+        }
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         var raycastResult = eventData.pointerCurrentRaycast;
-        Debug.Log("You have clicked" + raycastResult.gameObject.GetComponent<Renderer>().material);
+        //Debug.Log("You have clicked" + raycastResult.gameObject.GetComponent<Renderer>().material);
 
         var controller = GameObject.Find("VRAvatar").GetComponent<PlayerController>();
-        controller.colourSelection = raycastResult.gameObject.GetComponent<Renderer>().material;
-        curentHeldColour.gameObject.GetComponent<Renderer>().material = raycastResult.gameObject.GetComponent<Renderer>().material;
+        controller.colourSelection = currentMaterial; //raycastResult.gameObject.GetComponent<Renderer>().material;
+        curentHeldColour.gameObject.GetComponent<Renderer>().material = currentMaterial; //raycastResult.gameObject.GetComponent<Renderer>().material;
         tileHomer.gameObject.transform.position = eventData.pointerCurrentRaycast.worldPosition;
     }
 
     //Detect current clicks on the GameObject (the one with the script attached)
     public void OnPointerDown(PointerEventData eventData)
     {
-        this.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
+        //this.gameObject.GetComponent<Renderer>().material.EnableKeyword("_EMISSION");
     }
 
     //Detect if clicks are no longer registering
     public void OnPointerUp(PointerEventData eventData)
     {
-        this.gameObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
+        //this.gameObject.GetComponent<Renderer>().material.DisableKeyword("_EMISSION");
     }
 
 }
