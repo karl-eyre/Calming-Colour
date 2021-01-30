9f673ed85c614da7aa283f8640401ff218704262
Karl Eyre <karl_eyre@hotmail.com>
2021-01-31 02:12:45 +1100

    UI Tooltips loader.

---
+++ Calming Colour/Assets/Scripts/LoadTooltips.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LoadTooltips : MonoBehaviour, IPointerClickHandler
+{
+    public List<GameObject> tooltips;
+
+    // Update is called once per frame
+    void Start()
+    {
+        TooltipSwitch(true);
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        TooltipSwitch(true);
+    }
+
+    void TooltipSwitch(bool choice)
+    {
+        for (int i = 0; i < tooltips.Count; i++)
+        {
+            tooltips[i].SetActive(choice);
+        }
+    }
+}
+
+
